72ce71e529af8d47b497470bda98c8106e148d9c
Geetesh <singh.geetesh98@gmail.com>
2021-10-14 23:48:33 +0530

    added pagination

--- src/App.js
+++ src/App.js
@@ -1,59 +1,63 @@
-import * as React from 'react'
-import './App.css'
-
+import * as React from "react";
+import "./App.css";
 import directory from './directory.jpg'
 
 import { Button } from '@progress/kendo-react-buttons'
 
-const RemoveRequest =() => {
+const RemoveRequest = (users) => {
   alert('deleted')
 }
 
-const App= () => {
-  const [users, setUsers] = React.useState([])
-  const API_NAME = async () => {
-    const res = await fetch('https://reqres.in/api/users?page=2')
-    const json = await res.json()
-    setUsers(json.data)
-  }
+export default function App() {
+  const [users, setUsers] = React.useState([]);
+  const [canLoadMore, setCanLoadMore] = React.useState(true);
+  const [page, setPage] = React.useState(1);
+  const fakeData = async (page) => {
+    const res = await fetch(
+      `https://reqres.in/api/users/?page=${page}`
+    );
+    const json = await res.json();
+    setCanLoadMore(json.total_pages > page);
+    setUsers((data) => (page > 1 ? [...data, ...json.data] : json.data));
+  };
   React.useEffect(() => {
-    API_NAME()
-  }, [])
-
-  
-
-  
+    fakeData(page);
+  }, [page]);
   return (
     <div className='App'>
-    <div className='Header'>
-      <img src={directory} alt='banner' height='300px' width='99%'></img>
-    </div>
+      <div className='Header'>
+        <img src={directory} alt='banner' height='300px' width='99%'></img>
+      </div>
       <div className='flex'>
         {users.length &&
           users.map(user => {
             return (
               <div key={user.id}>
+                <img
+                  key={user.avatar}
+                  src={user.avatar}
+                  alt='avatar'
+                  height='120px'
+                />
+
                 <p>
                   <strong>{user.first_name}</strong>
                 </p>
                 <p>{user.email}</p>
-                <img key={user.avatar} src={user.avatar} alt='user-profile' />
-                <p>{user.support}</p>
-                <br />
-                <Button
-                  icon='delete'
-                  primary={true}
-                  onClick={RemoveRequest}
-                >
+                
+                <Button icon='delete' primary={true} onClick={RemoveRequest}>
                   DELETE
                 </Button>
+                <br />
               </div>
             )
           })}
       </div>
+      {canLoadMore && (
+        <div>
+          <button onClick={() => setPage(page => page + 1)}>Load More</button>
+        </div>
+      )}
     </div>
   )
 }
-
-
-export default App
